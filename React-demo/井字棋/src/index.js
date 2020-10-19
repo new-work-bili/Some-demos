e874177d6a6571c548efaccdc865be1b9c53b3ac
LPP <1655170926@qq.com>
2020-10-19 20:54:18 +0800

    #5

--- React-demo/井字棋/src/index.js
+++ React-demo/井字棋/src/index.js
@@ -13,6 +13,7 @@ class Game extends React.Component {
         {
           squares: Array(9).fill(null),
           clickZuobiao: 0,
+          index: 0,
         },
       ],
       xIsNext: true,
@@ -21,17 +22,35 @@ class Game extends React.Component {
       hiddenHistory: null,
       clickButton: "当前升序，点击降序",
       isP: false, //是否平局
+      historyIndex: 0,
     };
   }
   //子级点击触发的事件，其实就是子级反馈点击了那个方块i,进而改变父级state中的记号数组squarse
   handleClick(i) {
     //此处为了是跳转之前的历史之后再进行下棋，那么之前未来下的棋就不算了
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const history = this.state.history.slice(0, this.state.stepNumber + 1); //!!!!
     const current = history[history.length - 1]; //当前的
     const squares = current.squares.slice();
+    let historyIndex = ++this.state.historyIndex; //#4为了历史记录的响应顺序是死的；这样就算反转了也不会混乱
 
-    if (calculateWinner(squares) || squares[i]) {
-      //以判断获胜或者当前点击的方块已被点击
+    let addHistory =
+      this.state.clickButton === "当前升序，点击降序"
+        ? history.concat([
+            {
+              squares: squares,
+              zuobiaoIndex: i,
+              index: historyIndex,
+            },
+          ])
+        : [
+            {
+              squares: squares,
+              zuobiaoIndex: i,
+              index: historyIndex,
+            },
+          ].concat(history);
+    //以判断获胜或者当前点击的方块已被点击
+    if (calculateWinner(squares)[0] || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? "X" : "O";
@@ -39,12 +58,7 @@ class Game extends React.Component {
     this.setState(
       {
         xIsNext: !this.state.xIsNext, //每次下棋过后换边
-        history: history.concat([
-          {
-            squares: squares,
-            zuobiaoIndex: i,
-          },
-        ]),
+        history: addHistory,
         stepNumber: history.length, //下过棋时，当前的历史记录自然是当前最新的一步棋
         hiddenHistory: null,
       },
@@ -66,19 +80,27 @@ class Game extends React.Component {
   }
 
   //跳转历史记录
-  jumpTo(item, step) {
+  jumpTo(item, step, desc) {
     this.setState({
       stepNumber: step,
       xIsNext: step % 2 === 0,
       hiddenHistory: item.zuobiaoIndex === undefined ? null : item.zuobiaoIndex, //#2
+      historyIndex: desc, //#4:重置
     });
   }
 
   //#4
-  //changeHistory数组
+  //点击按钮反转history数组
   changeHistory = () => {
     let history = this.state.history.reverse();
-    this.setState(history);
+    let clickButton =
+      this.state.clickButton === "当前降序，点击升序"
+        ? "当前升序，点击降序"
+        : "当前降序，点击升序";
+    this.setState({
+      history,
+      clickButton,
+    });
   };
 
   render() {
@@ -86,11 +108,14 @@ class Game extends React.Component {
     //使用最新一次历史记录来确定并展示游戏的状态：
     const history = this.state.history; //就是一个数组，每次点击之后都把当前的9个格子的状态(这个是一个数组，9个方块9个值)push进history
     const current = history[this.state.stepNumber]; //current放的是当前展示的9格子的状态（有可能是最新点击之后的，也有可能是选择历史记录的）这一切都由step决定(就是步数)；进而去取history里面的值
-    const winner = calculateWinner(current.squares);
+    const winner = calculateWinner(current.squares)[0];
 
+    const winIndex = calculateWinner(current.squares)[1]; //#5:获胜位置高亮
     //map history，进而循环出历史记录
     const moves = history.map((item, index) => {
-      const desc = index ? "回退至 #" + index : "开始游戏";
+      // const desc = index ? "回退至 #" + index : "开始游戏";
+      //#4;
+      const desc = item.index ? "回退至 #" + item.index : "开始游戏";
 
       //#1;记录对应坐标到history
       const zuobiaoArr = [
@@ -107,7 +132,7 @@ class Game extends React.Component {
       const zuobiao = zuobiaoArr[item.zuobiaoIndex];
       return (
         <li key={index}>
-          <button onClick={() => this.jumpTo(item, index)}>
+          <button onClick={() => this.jumpTo(item, index, item.index)}>
             {desc} {zuobiao}
           </button>
         </li>
@@ -118,6 +143,7 @@ class Game extends React.Component {
     let status;
     if (winner) {
       status = "Winner: " + winner;
+      //高亮获胜旗子的位置
     }
     if (this.state.isP) {
       status = "平局";
@@ -131,6 +157,7 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             hiddenHistory={this.state.hiddenHistory}
+            winIndex={winIndex}
             onClick={(i) => {
               //通过i，堆每个方块进行实践绑定
               this.handleClick(i);
@@ -149,16 +176,6 @@ class Game extends React.Component {
 
 //该组件组成9个方块，并绑定点击事件和绑定选中状态（通过props
 class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]} //X/O;向上寻找Board组件的props
-        onClick={() => this.props.onClick(i)}
-        className={i === this.props.hiddenHistory ? "hiddenHistory" : null}
-      />
-    );
-  }
-
   render() {
     //#3：循环组件
     var itemArr = [];
@@ -171,6 +188,11 @@ class Board extends React.Component {
           value={this.props.squares[item]} //X/O;向上寻找Board组件的props
           onClick={() => this.props.onClick(item)}
           className={item === this.props.hiddenHistory ? "hiddenHistory" : null}
+          winIndex={
+            this.props.winIndex && this.props.winIndex.indexOf(item) != -1
+              ? "winIndex"
+              : null
+          }
           key={item}
         />
       );
@@ -183,7 +205,10 @@ class Board extends React.Component {
 //Square:设置了border的单个方块
 function Square(props) {
   return (
-    <button className={`square ${props.className}`} onClick={props.onClick}>
+    <button
+      className={`square ${props.className} ${props.winIndex}`}
+      onClick={props.onClick}
+    >
       {/* 点击事件向上找绑定的props */}
       {props.value} {/* 显示X/O;props向上寻找 */}
     </button>
@@ -207,8 +232,8 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return [squares[a], [a, b, c]]; //#5
     }
   }
-  return null;
+  return [null];
 }
