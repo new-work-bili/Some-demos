d0325185facc11bccbaf0156d2d9da48ca52e72c
LPP <1655170926@qq.com>
2020-11-28 23:01:28 +0800

    安全方面

--- React-demo/脚手架test/src/pages/cookie-test/Student.js
+++ React-demo/脚手架test/src/pages/cookie-test/Student.js
@@ -1,37 +1,47 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent } from "react";
 
 export default class Student extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            resData: "",
+        };
+    }
     //发ajax
-    sendAjax = ()=>{
+    sendAjax = () => {
         var url = "http://localhost:3001/get_S/";
 
         const xhr = new XMLHttpRequest();
         xhr.withCredentials = true;
         xhr.open("POST", url, true);
-        // xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded")
         xhr.setRequestHeader("Content-type", "application/json");
         xhr.send();
 
-
+        const that = this;
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
-                    console.log('成功！');
+                    var resData = JSON.parse(xhr.responseText);
+                    console.log("成功！", resData);
+                    that.setState({
+                        resData: resData.data,
+                    });
                 } else {
-                    console.log("error", xhr);
+                    console.log("error", resData);
                 }
             }
         };
+    };
+    componentDidMount() {
+        this.sendAjax();
     }
-    componentDidMount(){
-        this.sendAjax()
-    }
-    
+
     render() {
         return (
             <div>
                 <h1>这里是学生</h1>
+                <h2 style={{ color: "red" }}>获取数据：{this.state.resData}</h2>
             </div>
-        )
+        );
     }
 }
