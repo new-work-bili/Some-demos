0319e244d00078ed7a7a4e4d9506b00da059ba34
LPP <1655170926@qq.com>
2020-12-10 23:13:20 +0800

    no message

--- React-demo/脚手架test/src/pages/hook.js
+++ React-demo/脚手架test/src/pages/hook.js
@@ -5,11 +5,20 @@ function Test(props) {
     const [other, setOther] = useState("other");
     const [other2, setOther2] = useState("other3");
     let is = true;
-    //componentDidUpdate和componentDidMount的。
+    //在每轮渲染之后执行
+    //相当于componentDidUpdate和componentDidMount
     useEffect(() => {
         //变化时调用
         console.log(init, other, other2);
-    });
+
+        return ()=>{
+            //return的这个函数会在组件注销时调用，相当于componentWillUnmount
+            console.log('return Func');
+        }
+
+        //如果配置了依赖项，也就是useEffect的第二个参数，那么useEffect只会在useEffect改变时才会重新执行
+    },[init]);  
+
     console.log('hook:',props.location.search,props.location.state);
     const fn = () => {
         setOther(other + 2);
