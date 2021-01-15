a27a4e85002b452438e32cbb89e0de7816e87f9a
LPP <1655170926@qq.com>
2021-01-15 19:09:24 +0800

    no message

--- React-demo/脚手架test/src/components/Ref/forwardRef.js
+++ React-demo/脚手架test/src/components/Ref/forwardRef.js
@@ -5,5 +5,16 @@ import React from 'react'
       {props.children}
     </button>
   ));
+
+  const MyComponents = {
+    DatePicker: function DatePicker(props) {
+      return <div>Imagine a {props.color} datepicker here.</div>;
+    }
+  }
+  
+  function BlueDatePicker() {
+    const D = MyComponents['DatePicker']
+    return <D color="blue" />;
+  }
   
 export default FancyButton;
